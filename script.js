d25cfdfd70fee3e1fdacfe4f2fd8aa7106e8fcbd
pranay9949 <139853595+pranay9949@users.noreply.github.com>
2023-09-08 11:25:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,10 +4,18 @@ function fibonacci(num) {
 	let n2=1;
 	let i=3;
 	let n3;
+	if(num==1){
+		return 0;
+	}
+	if(num==2){
+		return 1;
+		
+	}
 	while(i<=num){
 		n3=n1+n2;
 		n1=n2;
 		n2=n3;
+		i++;
 	}
 	return n3;
 	
