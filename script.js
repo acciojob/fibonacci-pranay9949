be9ab285b1b1125d94bbdf3c64410ae008b83ec1
pranay9949 <139853595+pranay9949@users.noreply.github.com>
2023-09-08 11:20:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ function fibonacci(num) {
 // your code here
 	let n1=0;
 	let n2=1;
-	let i=1;
+	let i=3;
 	let n3;
 	while(i<=num){
 		n3=n1+n2;
 		n1=n2;
 		n2=n3;
 	}
-	return n3
+	return n3;
 	
 }
 
